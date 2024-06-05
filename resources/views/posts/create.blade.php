79f73f6f3a9eb729f232c75915a56e8238031b9e
Jose Miguel Dueñas <jomiguel097@gmail.com>
2024-06-04 21:44:14 -0500

    :sparkles: I install Dropzonejs, configure it and add it in the create post form. I remove bootstrap.js

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,7 +7,7 @@
 @section('contenido')
     <div class="md:flex md:items-center">
         <div class="md:w-1/2 px-10">
-            Imagen aquí
+            <form id="dropzone" action="/img" class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center"></form>
         </div>
         <div class="md:w-1/2 p-6 bg-white rounded-lg shadow-xl mt-10 md:mt-0">
             <form action="{{ route('register') }}" method="POST" autocomplete="off" novalidate>
