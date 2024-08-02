aa838d6abfbf39826bd48716732c35e871207ab8
Jose Miguel Dueñas <jomiguel097@gmail.com>
2024-08-02 12:15:18 -0500

    :package: Add package intervention for images manage

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Laravel\Facades\Image;
 
 class ImagenController extends Controller
 {
@@ -13,7 +15,18 @@ class ImagenController extends Controller
         // $input = $request->all();
 
         $imagen = $request->file('file');
+
+        // Genero un identificador único para cada imagen
+        $nombre_imagen = Str::uuid() . "." . $imagen->extension();
+
+        // Manejo de la imagen con Intervention Image
+        $imagen_servidor = Image::make($imagen);
+        // Defino tamaño de la imagen
+        $imagen_servidor->fit(1000, 1000);
+        // Guardo la imagen
+        $imagen_path = public_path('uploads') . '/' . $nombre_imagen;
+        $imagen_servidor->save($imagen_path);
  
-        return response()->json(['imagen' => $imagen->extension()]);
+        return response()->json(['imagen' => $nombre_imagen]);
     }
 }
